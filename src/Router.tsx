e83e8d0f265f6a586b59f584188131903d178c3f
Remerson Monteiro <kairemerson@gmail.com>
2025-04-04 15:06:37 -0300

    feat: created components router

---
+++ src/Router.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./page/Home/Home";
+import { History } from "./page/History";
+import { DefaultLayout } from "./layouts/DefaultLayout";
+
+
+export function Router (){
+    return (
+        <Routes>
+            <Route path="/" element={<DefaultLayout/>}>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/history" element={<History/>}/>
+
+            </Route>
+        </Routes>
+    )
+}
