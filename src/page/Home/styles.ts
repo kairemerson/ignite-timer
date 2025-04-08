b75655128f4e3326343df66fb38276d635675fca
Remerson Monteiro <kairemerson@gmail.com>
2025-04-08 15:32:15 -0300

    style: add styles

--- src/page/Home/styles.ts
+++ src/page/Home/styles.ts
@@ -15,39 +15,46 @@ export const HomeContainer = styled.main`
     }
 `
 
-export const FormContainer = styled.div`
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 0.5rem;
-    color: ${props=> props.theme["gray-100"]};
-    font-size: 1.125rem;
-    font-weight: bold;
-    flex-wrap: wrap;
+
+
+
+
+export const BaseCountdownButton = styled.button`
+ width: 100%;
+ border: 0;
+ padding: 1rem;
+ border-radius: 8px;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+
+ gap: 0.5rem;
+ font-weight: bold;
+ cursor: pointer;
+ color: ${props => props.theme["gray-100"]};
+ 
+
+ 
+ &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+ }
+ 
 `
 
-export const CountdownContainer = styled.div`
-    font-family: "Roboto-Mono", monospace;
-    font-size: 10rem;
-    line-height: 8rem;
+export const StartCountdownButton = styled(BaseCountdownButton)`
+    background: ${props => props.theme["gray-500"]};
     color: ${props => props.theme["gray-100"]};
 
-    display: flex;
-    gap: 1rem;
-    
-    span {
-        background: ${props => props.theme["gray-700"]};
-        padding: 2rem 1rem;
-        border-radius: 8px;
+    &:not(:disabled):hover {
+        background: ${props => props.theme["green-700"]};
     }
 `
 
-export const Separator = styled.div`
-    padding: 2rem 0;
-    color: ${props => props.theme["green-500"]};
-    width: 4rem;
-    overflow: hidden;
-    display: flex;
-    justify-content: center;
+export const StopCountdownButton = styled(BaseCountdownButton)`
+    background: ${props => props.theme["red-500"]};
+
+    &:not(:disabled):hover {
+        background: ${props => props.theme["red-700"]};
+    }
 `
